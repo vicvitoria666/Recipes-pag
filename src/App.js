9544fb420ba7e8f96350e03c16b1a0e5e5c4853e
Lucas-de-Souza-Pinto <lucasdesouzapinto2000@gmail.com>
2022-06-27 17:46:56 -0300

    atualização da main

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "./components/Main";
-import GlobalStyle from "styled-components";
-
+const GlobalStyle = createGlobalStyle `
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  list-style:none;
+}
+`
 export default function App() {
   return (
     <>
+      <GlobalStyle/>
       <Header/>
       <Main/>
       <Footer/>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,32 +1,60 @@
 import React from "react";
 import Card from "./Card";
+import styled from "styled-components";
+import RecipeImg1 from "../assets/foodiesfeed_detail-of-pavlova-strawberry-piece-of-cake.png"
+import RecipeImg2 from "../assets/foodiesfeed_neapolitan-pizza-margherita.png"
+import RecipeImg3 from "../assets/foodiesfeed_peanut-butter-and-banana-smoothie.png"
+import RecipeAbautImg from "../assets/pexels-karolina-grabowska.png"
+const MainContainer = styled.main`
+    width:100%;
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    background-color:#F2F4F1;
 
+`
+const Ul = styled.main`
+    display:flex;
+`
 export default function Main (){
+    const RecipesList = [
+        {recipeImg:RecipeImg1,recipeName:"Red Velvet Cake"},
+        {recipeImg:RecipeImg2,recipeName:"Margherita Pizza"},
+        {recipeImg:RecipeImg3,recipeName:"Peanut Smoothie"}
+    ]
     return(
         <>
-        <main>
+        <MainContainer>
             <section>
-            {/* {lista.map(item =>(
-                <Card 
-                Image={item}
-                ImageDescription={item}
-                Name={item}
-                Children={item}
-                />
-            ))} */}
+                <Ul>
+                    {RecipesList.map(item =>(
+                        <li>
+                            <Card
+                            Image={item.recipeImg}
+                            ImageDescription={`imagem do prato da receita de ${item.recipeName}`}
+                            Name={item.recipeName}
+                            />
+                        </li>
+                    ))}
+                </Ul>
             </section>
             <section>
-            <Card/>
+            <Card
+            Image={RecipeAbautImg}
+            ImageDescription={"sobre"}
+            Name={"Abault"}
+            Children={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tincidunt, tortor nec rhoncus dictum, lorem massa tempus sem, eu tincidunt libero velit sit amet velit. Nunc in euismod urna. Duis dapibus, elit eu eleifend tincidunt, nulla ipsum consectetur lorem, quis tempor lorem justo quis nisi. Nam interdum, nisi nec mollis sagittis, enim risus euismod nisi, quis rutrum quam augue id mauris. Pellentesque mattis hendrerit semper. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut vestibulum nisl ante, et ultricies sapien facilisis aliquam."}
+            />
             </section>
             <h2></h2>
             <h3></h3>
             <input/>
-            <button> </button>
+            <button>SUBMIT</button>
             <section>
 
             </section>
             
-        </main>
+        </MainContainer>
         </>
     )
 }
